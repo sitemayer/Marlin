4aecf5a20f82ac5898a9cff5e8bc1b2d349567be
sitemayer <sitemayerlee@163.com>
2016-06-20 17:36:44 +0800

    add a burnning firmware tool

--- Tools/burnning.sh
+++ Tools/burnning.sh
@@ -2,11 +2,15 @@
 
 if [ $# -lt 1 ]; then
     echo "Please excute this script like this:"
-    echo "  \$./burnning.sh hex mcu upload_port"
-else
+    echo "  |\$./burnning.sh mcu upload_port    | entry commandline mode"
+    echo "  |\$./burnning.sh hex mcu upload_port| entry burnning mode"
+elif [ $# -eq 2 ]; then
+    MCU=$1
+    PORT=$2
+    avrdude -C $AVRDUDE_CONF -P${PORT} -p${MCU} -c stk500v2 -t
+elif [ $# -eq 3 ]; then
     HEX=$1
     MCU=$2
     PORT=$3
-    avrdude -D -C $AVRDUDE_CONF -p${MCU} -P${PORT} -cwiring -b115200 -Uflash:w:${HEX}:i
+    avrdude -D -C $AVRDUDE_CONF -p${MCU} -P${PORT} -c stk500v2 -b115200 -Uflash:w:${HEX}:i
 fi
-
